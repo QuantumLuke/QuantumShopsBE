ef38d0abf4fe0cc04bc35e7faf9724e065acf083
QuantumLuke <lucadago26@gmail.com>
2025-07-16 10:03:47 +0200

    Initial commit

---
+++ src/main/java/com/quantumluke/quantumshops/QuantumShopsApplication.java
@@ -0,0 +1,13 @@
+package com.quantumluke.quantumshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantumluke/quantumshops/QuantumShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumluke.quantumshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
