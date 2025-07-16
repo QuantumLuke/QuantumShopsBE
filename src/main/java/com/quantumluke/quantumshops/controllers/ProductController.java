d857983b7eeb73b86f8978ef8a1a003ccd7b7a03
QuantumLuke <lucadago26@gmail.com>
2025-07-16 15:58:42 +0200

    product controller

--- src/main/java/com/quantumluke/quantumshops/controllers/ProductController.java
+++ src/main/java/com/quantumluke/quantumshops/controllers/ProductController.java
@@ -1,4 +1,120 @@
 package com.quantumluke.quantumshops.controllers;
 
+import com.quantumluke.quantumshops.exceptions.ProductNotFoundException;
+import com.quantumluke.quantumshops.models.Product;
+import com.quantumluke.quantumshops.request.AddProductRequest;
+import com.quantumluke.quantumshops.request.UpdateProductRequest;
+import com.quantumluke.quantumshops.response.ApiResponse;
+import com.quantumluke.quantumshops.services.product.IProductService;
+import jakarta.websocket.server.PathParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getAllProducts()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Product fetched successfully", productService.getProductById(id)));
+        } catch (ProductNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Product not found with id: " + id, e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching product: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<ApiResponse> getProductsByCategory(@PathVariable String category) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getProductsByCategory(category)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products by category: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/brand/{brand}")
+    public ResponseEntity<ApiResponse> getProductsByBrand(@PathVariable String brand) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getProductsByBrand(brand)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products by brand: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/category/{category}/brand/{brand}")
+    public ResponseEntity<ApiResponse> getProductsByCategoryAndBrand(@PathVariable String category, @PathVariable String brand) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getProductsByCategoryAndBrand(category, brand)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products by category and brand: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<ApiResponse> getProductsByName(@PathVariable String name) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getProductsByName(name)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products by name: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/brand/{brand}/name/{name}")
+    public ResponseEntity<ApiResponse> getProductsByBrandAndName(@PathVariable String brand, @PathVariable String name) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Products fetched successfully", productService.getProductsByBrandAndName(brand, name)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error fetching products by brand and name: ", e.getMessage()));
+        }
+    }
+
+
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Product added successfully", productService.addProduct(product)));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error adding product: ", e.getMessage()));
+        }
+    }
+
+    @PutMapping("{id}/update")
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable Long id, @RequestBody UpdateProductRequest productDetails) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("Product updated successfully", productService.updateProduct(productDetails, id)));
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Product not found with id: " + id, e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error updating product: ", e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("{id}/delete")
+    public ResponseEntity<ApiResponse> deleteProductById(@PathVariable Long id) {
+        try {
+            productService.deleteProductById(id);
+            return ResponseEntity.ok(new ApiResponse("Product deleted successfully", null));
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Product not found with id: " + id, e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error deleting product: ", e.getMessage()));
+        }
+    }
+
 }
