af8e2999a42df0b343e44a37fdfe4b7abaa276fa
QuantumLuke <lucadago26@gmail.com>
2025-07-17 16:38:47 +0200

    date fix

--- src/main/java/com/quantumluke/quantumshops/dto/OrderDto.java
+++ src/main/java/com/quantumluke/quantumshops/dto/OrderDto.java
@@ -3,14 +3,14 @@ package com.quantumluke.quantumshops.dto;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class OrderDto {
     private Long id;
     private long userId;
-    private LocalDateTime orderDate;
+    private LocalDate orderDate;
     private BigDecimal totalPrice;
     private String status;
     private List<OrderItemDto> orderItems;
