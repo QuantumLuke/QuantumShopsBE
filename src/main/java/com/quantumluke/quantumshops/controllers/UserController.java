ba7013416f6a463e883cb0e08ef71b5ec4489cbc
QuantumLuke <lucadago26@gmail.com>
2025-07-17 14:45:52 +0200

    user controller

---
+++ src/main/java/com/quantumluke/quantumshops/controllers/UserController.java
@@ -0,0 +1,68 @@
+package com.quantumluke.quantumshops.controllers;
+
+import com.quantumluke.quantumshops.exceptions.AlreadyExistsException;
+import com.quantumluke.quantumshops.exceptions.ResourceNotFoundException;
+import com.quantumluke.quantumshops.models.User;
+import com.quantumluke.quantumshops.request.CreateUserRequest;
+import com.quantumluke.quantumshops.request.UpdateUserRequest;
+import com.quantumluke.quantumshops.response.ApiResponse;
+import com.quantumluke.quantumshops.services.user.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/users")
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId) {
+        try {
+            User user = userService.getUserById(userId);
+            return ResponseEntity.ok(new ApiResponse("User retrieved successfully", user));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error retrieving user " + e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
+        try {
+            User user = userService.createUser(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("User created successfully", user));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error creating user: " + e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<ApiResponse> updateUser(@PathVariable Long userId, @RequestBody UpdateUserRequest request) {
+        try {
+            User user = userService.updateUser(userId, request);
+            return ResponseEntity.ok(new ApiResponse("User updated successfully", user));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error updating user: " + e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/{userId}/delete")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) {
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponse("User deleted successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error deleting user: " + e.getMessage(), null));
+        }
+    }
+}
